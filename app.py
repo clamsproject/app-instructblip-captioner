b15b28e27c96ce4a686c41bf55f9face0e961de2
kelleyl <kmlynch@brandeis.edu>
2024-06-26 14:55:36 -0400

    add prompt parameter, remove prints, add quantization

--- app.py
+++ app.py
@@ -2,26 +2,26 @@ import argparse
 import logging
 from typing import Union
 import torch
-from PIL import Image
-import requests
-from transformers import InstructBlipProcessor, InstructBlipForConditionalGeneration
-import numpy as np
-import cv2
+from transformers import InstructBlipProcessor, InstructBlipForConditionalGeneration, BitsAndBytesConfig
 
 from clams import ClamsApp, Restifier
-from mmif import Mmif, View, Annotation, Document, AnnotationTypes, DocumentTypes
-
-# For an NLP tool we need to import the LAPPS vocabulary items
-from lapps.discriminators import Uri
+from mmif import Mmif, View, Document, AnnotationTypes, DocumentTypes
 from mmif.utils import video_document_helper as vdh
 
 class InstructblipCaptioner(ClamsApp):
 
     def __init__(self):
-        self.model = InstructBlipForConditionalGeneration.from_pretrained("Salesforce/instructblip-vicuna-7b")
+        quantization_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype=torch.float8,
+        )
+        self.model = InstructBlipForConditionalGeneration.from_pretrained(
+            "Salesforce/instructblip-vicuna-7b", 
+            quantization_config=quantization_config, 
+            device_map="auto"
+        )
         self.processor = InstructBlipProcessor.from_pretrained("Salesforce/instructblip-vicuna-7b")
-        self.device = "cpu" if torch.cuda.is_available() else "cpu"
-        self.model.to(self.device)
         super().__init__()
 
     def _appmetadata(self):
@@ -30,39 +30,39 @@ class InstructblipCaptioner(ClamsApp):
         # When using the ``metadata.py`` leave this do-nothing "pass" method here. 
         pass
 
+    def get_prompt(self, label: str, prompt_map: dict, default_prompt: str) -> str:
+        prompt = prompt_map.get(label, default_prompt)
+        if prompt == "-":
+            return None
+        prompt = f"[INST] <image>\n{prompt}\n[/INST]"
+        return prompt
+
     def _annotate(self, mmif: Union[str, dict, Mmif], **parameters) -> Mmif:
-        print ("called annotate")
+        label_map = parameters.get('promptMap')
+        default_prompt = parameters.get('defaultPrompt')
+        frame_interval = parameters.get('frameInterval', 10)  # Default to every 10th frame if not specified
+
         video_doc: Document = mmif.get_documents_by_type(DocumentTypes.VideoDocument)[0]
         input_view: View = mmif.get_views_for_document(video_doc.properties.id)[0]
-
         new_view: View = mmif.new_view()
         self.sign_view(new_view, parameters)
-        print("Starting annotation process for timeframes.")
-        for timeframe in input_view.get_annotations(AnnotationTypes.TimeFrame):
-            try:
-                print(f"Processing timeframe: {timeframe.id}")
-                if "representatives" in timeframe.properties and timeframe.properties["representatives"]:
-                    representative_id = timeframe.get("representatives")[0]
-                    print(f"Found representative: {representative_id}")
-                    representative: AnnotationTypes.TimePoint = input_view.get_annotation_by_id(representative_id)
-                    frame_index = vdh.convert(representative.get("timePoint"), "milliseconds",
-                                            "frame", vdh.get_framerate(video_doc))
-                    print(f"Frame index for representative: {frame_index}")
-                else:
-                    start_frame = timeframe.get_property("start")
-                    end_frame= timeframe.get_property("end")
-                    if end_frame - start_frame < 30:
-                        continue
-                    print(f"Calculating frame index from start {start_frame}ms and end {end_frame}ms")
-                    frame_index = (start_frame + end_frame) // 2
-                    print(f"Frame index calculated: {frame_index}")
-                
-                image: Image.Image = vdh.extract_frames_as_images(video_doc, [frame_index], as_PIL=True)[0]
+        
+        timeframes = input_view.get_annotations(AnnotationTypes.TimeFrame)
+        
+        if timeframes:
+            for timeframe in timeframes:
+                label = timeframe.get_property('label')
+                prompt = self.get_prompt(label, label_map, default_prompt)
+                if not prompt:
+                    continue
 
-                prompt = "Describe this frame from a television program."
-                print(f"Using prompt: '{prompt}'")
-                inputs = self.processor(images=image, text=prompt, return_tensors="pt").to(self.device)
-                print("Inputs prepared for the model.")
+                representatives = timeframe.get("representatives") if "representatives" in timeframe.properties else None
+                if representatives:
+                    image = vdh.extract_representative_frame(mmif, timeframe)
+                else:
+                    image = vdh.extract_mid_frame(mmif, timeframe)
+
+                inputs = self.processor(images=image, text=prompt, return_tensors="pt")
                 outputs = self.model.generate(
                     **inputs,
                     do_sample=False,
@@ -74,19 +74,37 @@ class InstructblipCaptioner(ClamsApp):
                     temperature=1,
                 )
                 generated_text = self.processor.batch_decode(outputs, skip_special_tokens=True)[0].strip()
-                print (generated_text)
+
                 text_document = new_view.new_textdocument(generated_text)
-                self.create_alignment(new_view, timeframe.id, text_document.id)
+                alignment = new_view.new_annotation(AnnotationTypes.Alignment)
+                alignment.add_property("source", timeframe.id)
+                alignment.add_property("target", text_document.id)
+        else:
+            total_frames = vdh.get_frame_count(video_doc)
+            for frame_number in range(0, total_frames, frame_interval):
+                image = vdh.extract_frames_as_images(video_doc, [frame_number], as_PIL=True)[0]
+                prompt = default_prompt
+                inputs = self.processor(images=image, text=prompt, return_tensors="pt").to(self.device)
+                outputs = self.model.generate(
+                    **inputs,
+                    do_sample=False,
+                    num_beams=5,
+                    max_length=256,
+                    min_length=1,
+                    repetition_penalty=1.5,
+                    length_penalty=1.0,
+                    temperature=1,
+                )
+                generated_text = self.processor.batch_decode(outputs, skip_special_tokens=True)[0].strip()
 
-            except Exception as e:
-                self.logger.error(f"Error processing timeframe: {e}")
-                continue
+                text_document = new_view.new_textdocument(generated_text)
+                timepoint = new_view.new_annotation(AnnotationTypes.TimePoint)
+                timepoint.add_property("timePoint", frame_number)
+                alignment = new_view.new_annotation(AnnotationTypes.Alignment)
+                alignment.add_property("source", timepoint.id)
+                alignment.add_property("target", text_document.id)
+
         return mmif
-
-    def create_alignment(self, view, source_id, target_id):
-        alignment = view.new_annotation(AnnotationTypes.Alignment)
-        alignment.properties.source = source_id
-        alignment.properties.target = target_id
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
